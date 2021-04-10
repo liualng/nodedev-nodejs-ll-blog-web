552fd2b4f2afef7dc122a2df42e1181873e7cb65
liulanga <liulanga@zbj.com>
2021-04-10 17:32:33 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>你好</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.container{
+  h1{
+    color: red;
+  }
+}
+</style>
