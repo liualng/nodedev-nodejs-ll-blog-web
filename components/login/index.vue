8ca780e6ace0d110d4b88218837e849b6a4b90e2
liulanga <liulanga@zbj.com>
2021-04-26 12:51:39 +0800

    login

--- components/login/index.vue
+++ components/login/index.vue
@@ -13,9 +13,9 @@
     </el-carousel>
     <!-- 登陆框 -->
     <div class="login-index-components-content">
-      <div class="type-change-list">
+      <div :class="['type-change-list', typeNum === 1 ? 'active-item' : '']">
         <div
-          :class="['type-change-item', index === typeNum ? 'current-item' : '']"
+          class="type-change-item"
           v-for="(item, index) in types"
           :key="index"
           @click="setTypeNum(index)"
@@ -23,6 +23,7 @@
           {{ item.title }}
         </div>
       </div>
+      <!-- 用户名 -->
       <input
         type="text"
         id="name"
@@ -30,6 +31,7 @@
         placeholder="Enter Name"
         @keyup.enter="submitLogin"
       />
+      <!-- 密码 -->
       <input
         type="password"
         id="pwd"
@@ -37,6 +39,7 @@
         placeholder="Enter Password"
         @keyup.enter="submitLogin"
       />
+      <!-- 邮箱 -->
       <input
         v-show="typeNum"
         type="email"
@@ -44,6 +47,7 @@
         placeholder="Enter Email"
         @keyup.enter="submitLogin"
       />
+      <!-- 按钮 -->
       <div class="submit-login" @click="submitLogin">
         {{ types[typeNum].btnText }}
       </div>
@@ -163,10 +167,11 @@ export default {
     left: 50%;
     transform: translate(-50%, -50%);
     width: 460px;
+    height: 438px;
     padding: 30px 40px 50px 40px;
     background-color: #fff;
     .type-change-list {
-      display: flex;
+      position: relative;
       width: 340px;
       height: 50px;
       line-height: 50px;
@@ -175,14 +180,32 @@ export default {
       box-shadow: 0 1px 12px #ff6900ab;
       transition: all 0.3s;
       margin-bottom: 80px;
+      &.active-item {
+        &::before {
+          left: 170px !important;
+        }
+      }
       .type-change-item {
+        position: absolute;
+        z-index: 2;
         width: 170px;
         height: 100%;
         border-radius: 25px;
         cursor: pointer;
-        &.current-item {
-          background: linear-gradient(to right, #ff5e62, #ff9966);
+        &:last-child {
+          left: 170px;
         }
+      }
+      &::before {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        transition: left 0.5s;
+        content: "";
+        width: 170px;
+        height: 100%;
+        border-radius: 25px;
+        background: linear-gradient(to right, #ff5e62, #ff9966);
       }
     }
     input {
@@ -199,22 +222,52 @@ export default {
       outline: none;
     }
     .submit-login {
+      position: relative;
       margin: 0 auto;
       width: 200px;
       height: 48px;
       line-height: 48px;
       font-size: 20px;
       text-align: center;
-      background-image: linear-gradient(
-        56deg,
-        #a8591d,
-        #af5b4c,
-        #b35d77,
-        #b360a2
-      );
+      background-image: linear-gradient(to right, #25aae1, #40e495);
       color: #fff;
       cursor: pointer;
       border-radius: 30px;
+      &::before,
+      &::after {
+        position: absolute;
+        content: "";
+        border: 3px solid transparent;
+        width: 0px;
+        height: 0px;
+      }
+      &::before {
+        top: 0px;
+        left: 0px;
+      }
+      &::after {
+        bottom: 0px;
+        right: 0px;
+      }
+      &:hover {
+        letter-spacing: 4px;
+        &::after,
+        &::before {
+          width: 100%;
+          height: 100%;
+        }
+        &::before {
+          border-top-color: #fff;
+          border-right-color: #fff;
+          transition: width 0.3s ease-out, height 0.15s ease-out 0.3s;
+        }
+        &::after {
+          border-bottom-color: #fff;
+          border-left-color: #fff;
+          transition: border-color 0s ease-out 0.45s, width 0.3s ease-out 0.45s,
+            height 0.15s ease-out 1s;
+        }
+      }
     }
   }
 }
